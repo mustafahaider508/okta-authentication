ee52ad7fb7ca439c322e17c8b4aa5a13ecd7fa32
Mustafa <mustafahaider508@gmail.com>
2023-10-08 20:36:54 +0500

    initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Okta from "next-auth/providers/okta";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    Okta({
+      clientId: process.env.OKTA_CLIENTID as string,
+      clientSecret: process.env.CLIENT_SECERET as string,
+      issuer: process.env.OKTA_DOMAIN as string,
+    }),
+  ],
+  secret: process.env.SECERT as string,
+};
+
+export default NextAuth(authOptions);
