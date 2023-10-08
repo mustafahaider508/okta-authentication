a623bd8ebdacf8a50fe6618d9b896372b95feef8
Smad ul Hassan <smadulhassan@Smads-MacBook-Pro.local>
2023-10-08 23:04:03 +0500

    latest code

---
+++ src/pages/burger-dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Dashboard from "../../components/Dashboard";
+function index() {
+  return (
+    <div>
+      <Dashboard session={undefined} />
+    </div>
+  );
+}
+
+export default index;
